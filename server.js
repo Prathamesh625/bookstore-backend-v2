8613f4f3fe2941aea1107604a9f75b4fb087cac2
Prathamesh625 <92034832+Prathamesh625@users.noreply.github.com>
2022-12-21 12:42:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6500;
 
 app.use(
   cors({
